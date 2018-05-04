e57dd24437a895d901cef9ecee9e116f297c41d4
kudeh <kene.udeh@yahoo.com>
2018-05-03 21:35:22 -0300

    updated templates for comments and posts

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -42,14 +42,14 @@ class DraftListView(LoginRequiredMixin, ListView):
     model = Post
 
     def get_queryset(self):
-        return Post.objects.filter(published_date__isnull=True).orderby('created_date')
+        return Post.objects.filter(published_date__isnull=True).order_by('created_date')
 
 #####################################################
 @login_required
 def add_comment_to_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == 'POST':
-        form = CommentForm(request.Post)
+        form = CommentForm(request.POST)
         if form.is_valid():
             comment = form.save(commit=False)
             comment.post = post
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
     url(r'accounts/login$', views.login, name='login'),
-    url(r'accounts/logout/$', views.logout, kwargs={'next_page':'/'})
+    url(r'accounts/logout/$', views.logout, name='logout', kwargs={'next_page':'/'})
 ]
